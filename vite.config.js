3685c966c5acfc1b7017e9588998e7fcb0563afe
Shashank Gupta <80188495+shashankg687@users.noreply.github.com>
2025-10-15 12:03:08 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/PQChoiceAssistant',
+  base: '/PQCMigration',
   plugins: [
     vue(),
   ],
